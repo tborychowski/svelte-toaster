83a814b798cc1e1bce40cba6a9a8ca632a495db4
Tomasz Borychowski <tomasz.borychowski@sap.com>
2019-11-22 10:15:52 +0000

    initial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<style>
+	button:focus { box-shadow: 0 0 1px 1px blue; }
+</style>
+
+<Toaster />
+
+<button on:click="">Show toast</button>
+
+<script>
+import Toaster from './Toaster';
+
+
+
+</script>
